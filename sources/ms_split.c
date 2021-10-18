49767e7fb59a37e19e3d98e947ea30a91f796188
Tobias Grossmann <tgrossma@2-E-1.42heilbronn.de>
2021-10-18 17:27:48 +0200

    cleaned up and normed the ms_split

--- sources/ms_split.c
+++ sources/ms_split.c
@@ -6,12 +6,15 @@
 /*   By: tgrossma <tgrossma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 15:04:59 by tgrossma          #+#    #+#             */
-/*   Updated: 2021/10/18 16:37:34 by tgrossma         ###   ########.fr       */
+/*   Updated: 2021/10/18 17:26:16 by tgrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 
+/*
+//frees all elements of the given array, then the array itself
+*/
 static void	free_array(char **tab)
 {
 	int	i;
@@ -21,15 +24,16 @@ static void	free_array(char **tab)
 		return ;
 	while (tab[i])
 	{
-		printf("hello %s\n", tab[i]);
 		free(tab[i]);
 		tab[i] = NULL;
 		i++;
 	}
-	printf("hello world\n");
 	free(tab);
 }
 
+/*
+//returns the next argument to the string, keeping track of the index
+*/
 static char	*next_arg(char *line, int *index)
 {
 	char	*sub;
@@ -59,23 +63,20 @@ static char	*next_arg(char *line, int *index)
 	return (sub);
 }
 
-static int	count_arg(char *line)
+/*
+//returns the number of arguments in the given string
+*/
+static void	count_arg(char *line, int *c)
 {
-	int	count;
-	char quote;
-	int	i;
+	char	quote;
+	int		i;
 
-	if (!line)
-		return (0);
 	i = 0;
-	count = 1;
-	while (line[i] && line[i] == ' ')
-		i++;
-	while (line[i++])
+	while (line[i])
 	{
 		if (line[i] == ' ')
 		{
-			count++;
+			*c = *c + 1;
 			while (line[i] == ' ')
 				i++;
 		}
@@ -83,80 +84,69 @@ static int	count_arg(char *line)
 		{
 			quote = line[i];
 			i++;
-			while (line[i++] && line[i] != quote)
+			while (line[i] && line[i] != quote)
 			{
 				if (quote == '\"' && line[i] == '\\')
 					i++;
+				i++;
 			}
 		}
+		i++;
 	}
-	return (count);
 }
 
-// static char	**free_helper(char **array, char *arg)
-// {
-// 	if (arg)
-// 		free(arg);
-// 	if (array)
-// 		free_array(array);
-// 	return (NULL);
-// }
-
-
-// char **append_arg(char **array, char *arg)
-// {
-// 	int		i;
-// 	char	**tab;
-	
-// 	// if (!array || !arg)
-// 	// 	return (free_helper(array, arg));
-// 	i = 0;
-// 	while (array[i])
-// 		i++;
-// 	tab = (char **)ft_calloc(i + 2, sizeof(char *));
-// 	// if (!tab)
-// 	// 	return (free_helper(array, arg));
-// 	i = 0;
-// 	while (array[i])
-// 	{
-// 		tab[i] = array[i];
-// 		i++;
-// 	}
-// 	tab[i] = arg;
-// 	//free_array(array);
-// 	return (tab);
-// }
-
-
-int	ms_split(t_ms_data *ms_data)
+/*
+//fills the array with the substrings
+//returns 0 on success, 1 on error
+*/
+static int	fill_array(t_ms_data *ms_data, char *line, int c)
 {
-	int		n;
-	int		c;
-	int		i;	
-	char	**tab;
+	int	n;
+	int	i;
 
-	if (!ms_data || !ms_data->line)
-		return (1);
-	c = count_arg(ms_data->line);
-	tab = (char **)ft_calloc(c + 1, sizeof(char *));
 	n = 0;
 	i = 0;
 	while (i < c)
 	{
-		tab[i] = next_arg(ms_data->line, &n);
-		if (tab[i] == NULL)
+		ms_data->split_line[i] = next_arg(line, &n);
+		if (ms_data->split_line[i] == NULL)
 		{
-			free_array(tab);
-			tab = NULL;
+			free_array(ms_data->split_line);
+			ms_data->split_line = NULL;
+			free(line);
 			return (1);
 		}
 		i++;
 	}
-	i = 0;
-	while (tab[i])
+	return (0);
+}
+
+/*
+//takes a string and split it into the single arguments
+//returns 0 on success, 1 on error
+*/
+int	ms_split(t_ms_data *ms_data)
+{
+	char	*line;
+	int		c;
+
+	if (!ms_data || !ms_data->line)
+		return (1);
+	if (ms_data->split_line)
 	{
-		printf("%s\n", tab[i]);
-		i++;
+		free_array(ms_data->split_line);
+		ms_data->split_line = NULL;
 	}
+	line = ft_strtrim(ms_data->line, " ");
+	if (!line)
+		return (1);
+	c = 1;
+	count_arg(line, &c);
+	ms_data->split_line = (char **)ft_calloc(c + 1, sizeof(char *));
+	if (!ms_data->split_line)
+		return (1);
+	if (fill_array(ms_data, line, c) == 1)
+		return (1);
+	free(line);
 	return (0);
 }
