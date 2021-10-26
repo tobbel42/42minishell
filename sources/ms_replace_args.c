f5e2f2d008dfbbf958769f1c6700c650966b0275
Alice Kamlah <akamlah@2-G-2.42heilbronn.de>
2021-10-26 13:12:16 +0200

    changes

--- sources/ms_replace_args.c
+++ sources/ms_replace_args.c
@@ -6,7 +6,7 @@
 /*   By: akamlah <akamlah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 12:38:12 by akamlah           #+#    #+#             */
-/*   Updated: 2021/10/20 19:41:21 by akamlah          ###   ########.fr       */
+/*   Updated: 2021/10/26 13:05:47 by akamlah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,15 +82,49 @@ void	ms_replace_variable(t_ms_data *ms, int *i, char *token)
 int	ms_replace_args(t_ms_data *ms)
 {
 	int		i;
+	int j;
+	int qcl;
+	int qcr;
 	char	*line_cpy;
 	char	*token;
+	int		sqmap[ft_strlen(ms->line)];
+	int		dqmap[ft_strlen(ms->line)];
 
+	i = 0;
+	while(ms->line[i] != '\0')
+	{
+		if (ms->line[i] == '\'')
+			sqmap[i] = 1;
+		else
+			sqmap[i] = 0;
+		if (ms->line[i] == '\"')
+			dqmap[i] = 1;
+		else
+			dqmap[i] = 0;
+		i++;
+	}
 	i = 0;
 	while(ms->line[i] != '\0')
 	{
 		if ((ms->line[i] == '$') && (ms->line[i + 1] != '\0' && \
 			ms->line[i + 1] != '$' && ft_isspace(ms->line[i + 1]) != 1))
 		{
+			j = i;
+			qcl = 0;
+			while (j >= 0 && sqmap[j] == '\'')
+			{
+				qcl += sqmap[j];
+				j--;
+			}
+			j = i;
+			qcr = 0;
+			while(ms->line[j] != '\0'  && sqmap[j] == '\'')
+			{
+				qcr += sqmap[j];
+				j++;
+			}
+			if (qcr == qcl)
+				break ;
 			line_cpy = ms->line + i;
 			token = ms_get_next_token(line_cpy);
 			// if (token[ft_strlen(token) - 1] == '\'' \
@@ -105,3 +139,7 @@ int	ms_replace_args(t_ms_data *ms)
 	// printf("i: %d LINE: %s\n", i, ms->line);
 	return (0);
 }
+
+// 3.5.9 Quote Removal
+// After the preceding expansions, all unquoted occurrences of the characters ‘\’, 
+// ‘'’, and ‘"’ that did not result from one of the above expansions are removed.
