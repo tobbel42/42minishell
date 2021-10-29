5f2d61a17c2ab5bae82df1bde616ba0cc297a009
Alice Kamlah <akamlah@1-C-15.42heilbronn.de>
2021-10-29 16:19:22 +0200

    changes is builtins

--- sources/ms_builtin_env.c
+++ sources/ms_builtin_env.c
@@ -7,15 +7,10 @@
 */
 int	ms_builtin_env(t_ms_data *ms, t_ms_task *task)
 {
-	char	*tmp;
-
-	tmp = NULL;
 	if (task->args[1] != NULL)
 	{
 		task->err_flag = 1;
-		tmp = ft_strjoin("env: ", task->args[1]);
-		task->err_msg = ft_strjoin(tmp, ": No such file or directory\n");
-		free(tmp);
+		task->err_msg = ft_strjoin(task->args[1], ": No such file or directory\n");
 		return (-1);
 	}
 	ms_print_env_list(ms);
