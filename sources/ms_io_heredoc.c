975596805ae2d17e952080e917d2aafeb8794a63
Tobias Grossmann <tgrossma@1-C-14.42heilbronn.de>
2021-11-03 14:49:05 +0100

    some small changes

--- sources/ms_io_heredoc.c
+++ sources/ms_io_heredoc.c
@@ -7,20 +7,12 @@ void	ms_io_heredoc(t_ms_task	*task)
 	int		fd[2];
 
 	pipe(fd);
-	task->next->fd_in = fd[0];
-	ft_putchar_fd('>', 1);
-	line = get_next_line(0);
-	printf("%s\n", line);
-	while (line && ft_strncmp(task->args[1], line, (int)ft_strlen(task->args[1]) + 1))
+	int		len = (int)ft_strlen(task->args[1]);
+	line = readline("> ");
+	while (line && ft_strncmp(task->args[1], line, len))
 	{
-		ft_putchar_fd('>', 1);
-		ft_putstr_fd(line, fd[1]);
-		if (line)
-			free (line);
-		line = get_next_line(0);
-		printf("%s\n", line);
-	}
-	if (line)
+		ft_putendl_fd(line, fd[1]);
 		free(line);
+	}
 	close(fd[1]);
 }
