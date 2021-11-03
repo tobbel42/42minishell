cb22a44ffd4c4dfedd48f9f427b8a0b199902156
Tobias Grossmann <tgrossma@1-C-14.42heilbronn.de>
2021-11-03 15:01:06 +0100

    fixed some bugs

--- sources/ms_io_heredoc.c
+++ sources/ms_io_heredoc.c
@@ -5,14 +5,38 @@ void	ms_io_heredoc(t_ms_task	*task)
 	//toDo: launch as seperate process, different check for exit, safty
 	char	*line;
 	int		fd[2];
+	int		len;
 
-	pipe(fd);
-	int		len = (int)ft_strlen(task->args[1]);
+	if (pipe(fd))
+	{
+		return ;
+	}
+	len = (int)ft_strlen(task->args[1]);
 	line = readline("> ");
 	while (line && ft_strncmp(task->args[1], line, len))
 	{
 		ft_putendl_fd(line, fd[1]);
 		free(line);
+		line = readline("> ");
 	}
+	if (line)
+		free(line);
 	close(fd[1]);
+	t_ms_task *node = task;
+	while (node && node->prev && ft_strncmp("|", node->prev->name, 2))
+	{
+		if (ms_is_cmd(node->name))
+		{
+			task->fd_in = fd[0];
+			return ;
+		}
+		node = node->prev;
+	}
+	while (node && t_strncmp("|", node->name, 2))
+	{
+		if (ms_is_cmd(node->name))
+		node = node->next;
+	}
+	
+	
 }
