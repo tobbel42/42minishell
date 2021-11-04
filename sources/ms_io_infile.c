98318d6a68d24f1175df685066cd98a17fc9358e
Tobias Grossmann <tgrossma@1-C-14.42heilbronn.de>
2021-11-04 14:55:20 +0100

    ms_io_infile.c now works in multibles and position independant

--- sources/ms_io_infile.c
+++ sources/ms_io_infile.c
@@ -1,23 +1,48 @@
 #include "../header/minishell.h"
 
+static t_ms_task	*infile_finder(t_ms_task *task)
+{
+	t_ms_task	*node;
+
+	node = task;
+	while (node && node->prev && ft_strncmp("|", node->prev->name, 2))
+	{
+		if (ms_is_cmd(node->name))
+			return (node);
+		node = node->prev;
+	}
+	while (node && ft_strncmp("|", node->name, 2))
+	{
+		if (ms_is_cmd(node->name))
+			return (node);
+		node = node->next;
+	}
+	return (NULL);
+}
+
 /*
 //tries to open the infile, and passes the fd to the next cmd
-//if the file cant be open whatsoever an empty pipe is passed to the nxt cmd
 */
 int	ms_io_infile(t_ms_task *task)
 {
-	int	fd;
+	int			fd;
+	t_ms_task	*target;
 
 	fd = open(task->args[1], O_RDONLY);
-	
+	target = infile_finder(task);
 	if (fd == -1)
 	{
 		task->err_flag = 1;
 		task->err_msg = ft_strdup(strerror(errno));
-		if (task->next)
-			task->next->err_flag = 2;
+		//maby, if multi file open error and no cmd just 1 err msg, now for each
+		if (target)
+			target->err_flag = 2;
 	}
-	else if (task->next)
-		task->next->fd_in = fd;
+	else if (target)
+	{
+		if (target->fd_in != 0)
+			close(target->fd_in);
+		target->fd_in = fd;
+	}
 	return (0);
 }
