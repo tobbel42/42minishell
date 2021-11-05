9680ff032bb3fce7d894f46adeec8cfb5dc85147
Alice Kamlah <akamlah@1-B-15.42heilbronn.de>
2021-11-05 18:43:49 +0100

    fixed echo

--- sources/ms_builtin_echo.c
+++ sources/ms_builtin_echo.c
@@ -3,7 +3,7 @@
 static int	ms_is_nflag(char *str)
 {
 	int	i;
-	
+
 	if (str[0] != '-')
 		return (0);
 	i = 1;
@@ -20,18 +20,22 @@ static int	ms_is_nflag(char *str)
 	Function that writes to given fd the arguments following
 	the command "echo", with a final newline which is suppressed
 	if -n option is specified.
+	Does not handle ~.
 */
 int	ms_builtin_echo(t_ms_task *task)
 {
 	int	n_flag;
 	int	i;
+	int j;
 
 	n_flag = 0;
 	i = 1;
-	if (ms_is_nflag(task->args[1]) == 1)
+	j = 1;
+	while (task->args[j] && ms_is_nflag(task->args[j]) == 1)
 	{
 		n_flag = 1;
-		i = 2;
+		i = j + 1;
+		j++;
 	}
 	while (task->args[i])
 	{	
